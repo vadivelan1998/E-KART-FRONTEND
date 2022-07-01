7da72c22eabcd738c70d849f1d15834b1a0c2507
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-01 17:38:11 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store/store";
+import axios from "axios";
+import { theme } from "./theme";
+
+axios.defaults.baseURL = "http://localhost:5600/";
+
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ReduxProvider store={store}>
+        <Router>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </Router>
+      </ReduxProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
