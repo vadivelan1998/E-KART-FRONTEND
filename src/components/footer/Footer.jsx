4ca87bd27be531db7e5f013b4fafec4c2fcb1470
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-01 17:40:21 +0530

    updated components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import { Flex, Text, HStack, Button, VStack, Link } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+
+export const Footer = () => {
+
+    return (
+        <Flex h={'160px'} bg={'#1a202c'} color={'white'} justify={'center'} mt={20}>
+            <VStack>
+                <Text my={'26px'}>Made with ❤️ by Murali M V</Text>
+                <HStack>
+                    <Button variant={'solid'} bg={'#0863be'} size='sm' leftIcon={<FaLinkedin />}>
+                        <Link _hover={{ 'textDecoration': 'none' }} target={"_blank"} href={"https://www.linkedin.com/in/murali-m-v/"}>LinkedIn</Link>
+                    </Button>
+                    <Button _hover={{ 'bg': 'red.500' }} variant={'solid'} bg={'red'} size='sm' leftIcon={<FaGithub />}>
+                        <Link _hover={{ 'textDecoration': 'none' }} target={"_blank"} href={"https://github.com/murali0101/"}>GitHub</Link>
+                    </Button>
+                </HStack>
+            </VStack>
+        </Flex>
+    );
+};
