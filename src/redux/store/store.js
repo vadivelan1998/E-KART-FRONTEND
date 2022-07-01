a7f5926122adc16813a2ae68f0fc9b1627e4ba08
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-01 17:45:27 +0530

    redux updated

---
+++ src/redux/store/store.js
@@ -0,0 +1,20 @@
+import {  applyMiddleware, combineReducers, legacy_createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { allProductsReducer } from '../allProducts/reducer';
+import { authReducer } from '../auth/reducer';
+import { cartReducer } from '../cartProducts/reducer';
+import { favouriteReducer } from '../favouriteProducts/reducer';
+import { oneProductReducer } from '../oneProduct/reducer';
+import { orderReducer } from '../orderDetails/reducer';
+
+
+const rootReducer = combineReducers({
+    allProductsReducer,
+    oneProductReducer,
+    authReducer,
+    favouriteReducer,
+    cartReducer,
+    orderReducer
+});
+
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
